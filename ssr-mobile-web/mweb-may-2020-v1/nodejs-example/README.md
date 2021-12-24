9586615c89d849455cf0fc8731acf7169ccc99ca
Mark E. Pascual <mpascual@linkedin.com>
2021-12-24 10:45:45 -0800

    Example: Nodejs with SSR Mobile Web May 2020 Model V1.

---
+++ ssr-mobile-web/mweb-may-2020-v1/nodejs-example/README.md
@@ -0,0 +1,11 @@
+# Nodejs with SSR Mobile Web May 2020 Model V1
+
+This is a simple example to show how to create an API that makes
+performance quality predictions using Expressjs with Nodejs. It
+uses the SSR Mobile Web May 2020 V1 model.
+
+## How to Run the Example
+
+1. `npm install`
+2. `node server.js`
+3. In your browser, visit `http://localhost:3001`
---
+++ ssr-mobile-web/mweb-may-2020-v1/nodejs-example/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const { MWebMay2020Predictor } = require('./predictors');
+
+async function main() {
+  // load the model so it's ready
+  const predictor = new MWebMay2020Predictor(__dirname + '/../models/nodejs-saved-model');
+  await predictor.loadModel();
+
+  // Make all the files in 'www' available.
+  app.use(express.static('www'));
+
+  app.get('/api/performanceQuality', (req, res) => {
+    const {
+      asn,
+      country_code,
+      browser_name,
+      browser_major_version,
+      osFamily,
+      osMajor
+    } = req.query;
+    const result = predictor.predict({asn, country_code, browser_name, browser_major_version, osFamily, osMajor});
+    res.setHeader('Content-Type', 'application/json');
+    res.send(result);
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/www/index.html');
+  });
+
+  // Listen for requests.
+  const listener = app.listen(3001, () => {
+    console.log('Your app is listening on port ' + listener.address().port);
+  });
+}
+
+main();
+
